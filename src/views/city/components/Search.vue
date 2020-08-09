613615a50625005947674a3002a361703123035b
神经蛙 <470408436@qq.com>
2020-08-10 06:45:30 +0800

    city-search-layout

---
+++ src/views/city/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  </div>
+</template>
+
+<script>
+//import x from ''
+export default {
+  name: 'CitySearch',
+  data () {
+    return {
+    }
+  },
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.search
+  height 0.72rem
+  padding 0 0.1rem
+  background-color $bgColor
+  .search-input
+    box-sizing border-box
+    height 0.62rem
+    padding 0 0.14rem
+    line-height 0.62rem
+    width 100%
+    text-align center
+    border-radius 0.08rem
+    color #666
+</style>
