3597d0e03a95d3e251c31a4a0cb10e1d85acdbf6
神经蛙 <470408436@qq.com>
2020-08-10 20:25:25 +0800

    finish keepalive

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -16,6 +16,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -25,17 +26,24 @@ export default {
     HomeRecommend,
     HomeWeekend
   },
-  // 实例被挂载后调用，注意，mounte的不会保证所有的子组件一起被挂载
-  mounted () {
-    // 发起ajax请求
-    this.getHomeInfo()
+  data () {
+    return {
+      lastCity: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  computed: {
+    ...mapState(['city'])
   },
   methods: {
     getHomeInfo () {
-      axios.get('./api/index.json').then(this.getHomeInfoSucc)
+      axios.get('./api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
-      console.log(res)
+      // console.log(res)
       // 将ajax返回的数据绑定到本组件,ret 表示后端正确返回了结果
       res = res.data
       if (res.ret && res.data) {
@@ -46,14 +54,22 @@ export default {
       }
     }
   },
-  data () {
-    return {
-      swiperList: [],
-      iconList: [],
-      recommendList: [],
-      weekendList: []
-    }
+  // 实例被挂载后调用，注意，mounte的不会保证所有的子组件一起被挂载
+  mounted () {
+    // 发起ajax请求
+    this.lastCity = this.city
+    this.getHomeInfo()
   },
+  // 钩子函数：被keep-alive缓存的组件激活时调用
+  activated () {
+    // console.log("activated")
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      //  切换城市重新发送ajax请求
+      this.getHomeInfo()
+    }
+  }
+
 }
 </script>
 <style lang="stylus" scoped></style>
