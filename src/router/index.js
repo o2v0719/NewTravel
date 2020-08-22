3a6c6fd474b689ae743cfdb0adcf11abb551e1bb
神经蛙 <470408436@qq.com>
2020-08-22 16:03:15 +0800

    add-inform

--- src/router/index.js
+++ src/router/index.js
@@ -5,22 +5,27 @@ import City from '@/views/city/City'
 import Detail from '@/views/detail/Detail'
 Vue.use(VueRouter)
 
+// 测试：将异步组件实现按需加载？
+// 项目较小的时候没有必要异步加载
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+    // component: () => import('@/views/home/Home')
   },
   {
     path: '/city',
     name: 'City',
     component: City
+    // component: () => import('@/views/city/City')
   },
   {
     //动态路由
     path: '/detail/:id',
     name: 'Detail',
     component: Detail
+    // component: () => import('@/views/detail/Detail')
   }
 ]
 
